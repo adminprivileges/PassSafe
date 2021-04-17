7590b6a9d7b30cd2fbf5794ed98672a657bfa463
DOOM <caldwellthaddeus@protonmail.com>
2021-04-17 17:48:45 -0400

    changed for MySQL

--- pokemon.py
+++ pokemon.py
@@ -1,18 +1,20 @@
-import requests, re, sqlite3
+import requests, re, sqlite3, pyodbc
 from bs4 import BeautifulSoup
 try:
-    conn = sqlite3.connect(f"/home/th/code/python/passsafe/passsafe.db")
+    conn = pyodbc.connect(
+        f"DRIVER=ODBC Driver 17 for SQL Server; SERVER=192.168.4.145; DATABASE=Passsafe; UID=SA; PWD=######;"
+    )
     #Initialize the cursor Object
     cursor = conn.cursor()         
-    poke_table_create = f"""CREATE TABLE IF NOT EXISTS pokemon(
-    poke_id integer PRIMARY KEY,
-    poke_name text NOT NULL,
-    poke_type text NOT NULL,
-    poke_desc text NOT NULL          
-    )"""
-    cursor.execute(poke_table_create)
+    #poke_table_create = f"""CREATE TABLE IF NOT EXISTS pokemon(
+    #poke_id integer PRIMARY KEY,
+    #poke_name text NOT NULL,
+    #poke_type text NOT NULL,
+    #poke_desc text NOT NULL          
+    #)"""
+    #cursor.execute(poke_table_create)
     #I know theres more pokemon, but i dont want more than 550
-    for x in range(1,551):
+    for x in range(84,551):
         poke_url = f"https://pokemon.gameinfo.io/en/pokemon/{x}" 
         page = requests.get(poke_url) #Request html for whole page
         soup = BeautifulSoup(page.content, 'html.parser') #make it pretty
@@ -25,10 +27,14 @@ try:
         poke_type = poke_type[0][13:] #Remove POKEMON_TYPE_
         poke_desc = soup.find_all(class_="description")
         poke_desc = poke_desc[1].text.strip().replace("'", "") #single quotes in the string confuses SQL
-        poke_insert_logic = f'''INSERT INTO pokemon(poke_id, poke_name, poke_type, poke_desc) VAlUES("{x}", "{poke_name}", "{poke_type}", {poke_desc})'''
-        #print(poke_insert_logic)
+        #if len(poke_desc) > 128:
+        #    poke_desc = f"\"{poke_desc[1:120]}\""
+        poke_insert_logic = f"""INSERT INTO pokemon(pokedex_id, name, type, description) VAlUES('{x}', '{poke_name}', '{poke_type}', '{poke_desc[1:-1]}')\n"""
+        print(poke_insert_logic)
         cursor.execute(poke_insert_logic)
-        
+        #testfile = open('testtext.txt', 'a+')
+        #testfile.write(poke_insert_logic)
+        #testfile.close()
 except sqlite3.Error as e: 
     print(e) 
 
